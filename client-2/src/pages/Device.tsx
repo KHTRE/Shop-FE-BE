c591da34118e4eac9804bc8bbc630ec8bb0a1839
KHTRE <KHTREE@gmail.com>
2025-04-13 16:35:13 +0300

    Added router

---
+++ client-2/src/pages/Device.tsx
@@ -0,0 +1,8 @@
+export const Device = () => {
+
+  return (
+    <div>
+      Device
+    </div>  
+  );
+};
--- client-2/src/router/routes.tsx
+++ client-2/src/router/routes.tsx
@@ -1,4 +1,8 @@
 import { Admin } from "../pages/Admin"
+import { Auth } from "../pages/Auth"
+import { Basket } from "../pages/Basket"
+import { Device } from "../pages/Device"
+import { Shop } from "../pages/Shop"
 
 export enum ROUTES {
   Home = "/",
@@ -16,30 +20,25 @@ export const privateRoutes = [
   },
   {
       path: ROUTES.Basket,
-      Component: Admin
-      // Component: Basket
+      Component: Basket
   },
 ]
 
 export const publicRoutes = [
   {
     path: ROUTES.Home,
-    Component: Admin
-    // Component: Shop
+    Component: Shop
   },
   {
     path: ROUTES.Login,
-    Component: Admin
-    // Component: Auth
+    Component: Auth
   },
   {
     path: ROUTES.Registration,
-    Component: Admin
-    // Component: Auth
+    Component: Auth
   },
   {
     path: ROUTES.Device,
-    Component: Admin
-    // Component: DevicePage
+    Component: Device
   },
 ]
